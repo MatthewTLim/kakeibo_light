d0e48b8534bafdc768ae8289e6ebe354e5fcd571
Matthew Lim <125940571+MatthewTLim@users.noreply.github.com>
2025-01-11 13:59:02 -0800

    Initial commit

---
+++ src/main/java/com/kakeibo/kakeibo_light/KakeiboLightApplication.java
@@ -0,0 +1,13 @@
+package com.kakeibo.kakeibo_light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakeiboLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakeiboLightApplication.class, args);
+	}
+
+}
